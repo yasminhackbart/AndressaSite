58efc3c3b9d0e512e387b7769cef2fac0944cbe7
Yasmin Hackbart <90115526+yasminhackbart@users.noreply.github.com>
2026-08-26 15:52:02 -0300

    Update site-header.tsx

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,7 +1,7 @@
 import { Link } from "@tanstack/react-router";
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
-import Logo from "./routes/logo.png"
+import Logo from "./logo.png"
 
 const nav = [
   { to: "/", label: "Início" },
